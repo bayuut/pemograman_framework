ad190456c2c5fda87951ba9e81929e10fe93d38c
Bayu Utomo <bayuutomo.usm@gmail.com>
2022-10-04 11:23:03 +0700

    Ditambah getinfo dengan parameter

--- Lat_1.php
+++ Lat_1.php
@@ -4,16 +4,26 @@ class Mobil{
     public $nama;
     public $merk;
 
-    function getInfo(){
+   /* function getInfo(){
+        echo "Nama mobil:".$this->nama."<br/>";
+        echo "Merk : ".$this->merk."<br/>";
+    }*/
+    function getInfo($a){
         echo "Nama mobil:".$this->nama."<br/>";
         echo "Merk : ".$this->merk."<br/>";
     }
 }
 
 //bagian main
-$ferari = new Mobil ();
+$ferari = new Mobil();
 $ferari->nama="xxx";
 $ferari->merk="aaa";
 
-$ferari->getInfo();
+$ferari->getInfo($a);
+echo "<hr/>";
+$jeep = new Mobil();
+$jeep->nama="JEEP";
+$jeep->merk="bbbb";
+
+$jeep->getInfo($a);
 ?>
